e7a8b0088a5832cb004c6899c1e01429a9bcbb00
jonjeung8 <jon.jeung@gmail.com>
2023-04-25 15:06:44 -0500

    Initiated on-screen-keyboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
 function App() {
+
+  const KEYS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  
   return (
     <div className="App">
       <h1>Hangman</h1>
+      <div className="gallowsContainer"></div>
+      <div className="gameWordContainer"></div>
+      
+      <div className='keyboardContainer'>
+        {KEYS.map(key => {
+          return (
+            <button key={key}>{key}</button>
+          )
+        })}
+      </div>
     </div>
   );
 }

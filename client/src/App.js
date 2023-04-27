7ec323fe0332efff6676d87d13329ee99ea0e36f
Yvettecodinghard <y.yvette98k@gmail.com>
2023-04-26 21:17:31 -0700

    design a pop-up design function

--- client/src/App.js
+++ client/src/App.js
@@ -162,9 +162,8 @@ function App() {
   }
 
   // Handles each guess
-  const letterGuess = (letter) => {
+  const letterGuess = async (letter) => {
     let correctGuess = false;
-    
     const letterClicked = document.getElementById(letter)
     letterClicked.disabled = true;
     
@@ -173,21 +172,36 @@ function App() {
         const correctGuessesCopy = [...correctGuesses];
         correctGuessesCopy.push(letter);
         setCorrectGuesses(correctGuessesCopy);
-
         correctGuess = true;
       }
     }
-
+  
     if(!correctGuess) {
       setGuessesRemaining(guessesRemaining - 1);
-
-      // Update the hangman image based on the remaining guesses
       const currentImageIndex = Math.max(0, hangmanImages.length - guessesRemaining);
       setHangmanImage(hangmanImages[currentImageIndex]);
     }
-
+  
     setTotalGuesses(totalGuesses + 1);
-  }
+  
+    if (guessesRemaining === 1) {
+      try {
+        const definition = await getDefinition(gameWord);
+        alert(`Hint: ${definition}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }  
+
+  const handleGetHint = async () => {
+    try {
+      const definition = await getDefinition(gameWord);
+      alert(`Hint: ${definition}`);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const urlGenerator = (e) => {
     e.preventDefault();
@@ -280,6 +294,17 @@ function App() {
     </>
   )
 
+  async function getDefinition(word) {
+    const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=8be9f87e-3ee9-4572-8416-8cebdc1cfd92`);
+    const data = await response.json();
+    if (data.length > 0) {
+      return data[0].shortdef.join('\n');
+    } else {
+      throw new Error('Word not found');
+    }
+  }
+  
+
   // Game over screen
   const gameOverDisplay = (
     <div className='loserScreenContainer'>
@@ -295,7 +320,6 @@ function App() {
 
   return (
     <div className="App">
-
       {(leaderboardScores)
         ?
         <div className='leaderboardScreen'>
@@ -315,7 +339,7 @@ function App() {
           {winnerScreen /* Overlay for winner */}
           {loserScreen /* Overlay for loser */}
           {wordInputScreen}
-
+  
           <h1>Hangman</h1>
           <div className="gallowsContainer">
             <img src={hangmanImage} alt="hangman" />
@@ -323,8 +347,9 @@ function App() {
             
           <div>
             <p>Guesses Remaining: {guessesRemaining}</p>
+            {guessesRemaining === 1 && <button onClick={handleGetHint}>Hint</button>}
           </div>
-
+  
           <div className="gameWordContainer">
             {gameWordDisplay}
           </div>
@@ -336,7 +361,7 @@ function App() {
               )
             })}
           </div>
-
+  
           <div className='computerGameQuit'>
             <button onClick={quitGame}>Quit</button>
           </div>
@@ -344,6 +369,8 @@ function App() {
       }
     </div>
   );
+  
+ 
 }
 
 export default App;
